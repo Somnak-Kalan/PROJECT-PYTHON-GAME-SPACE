f31a63746c3f300ee91aded4df1c8b577c96ab1f
Somnak-Kalan <kalansomnak12@gmail.com>
2021-12-24 14:06:53 +0700

    make win and lose

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
-from random import randbytes, randrange
 import tkinter as tk
+import winsound
+from random import randbytes, randrange
 from tkinter import Button, Image, Label, constants
 from tkinter import font
 from typing import Text
@@ -9,21 +10,22 @@ frame=tk.Frame()
 frame.master.title("SPACE MINER")
 canvas=tk.Canvas(frame)
 img=tk.PhotoImage(file="image\space.png")
-canvas.create_image(400,400,image=img)
+# --------------add sound background-----------
+# winsound .PlaySound("sound\spacebackground.wav",winsound.SND_FILENAME | winsound.SND_ASYNC)
 # ------------------------grid constants----------------
 GRID=[
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,3,0],
+    [0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,3,0,0,0,3,0],
     [0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0],
-    [0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0],
+    [0,0,0,0,0,0,0,3,0,0,0,0,0,3,0,0,0,3,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0],
+    [0,0,0,3,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0],
+    [0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,3,0,0,0,0],
     [0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0],
-    [0,2,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0],
+    [0,0,0,0,3,0,3,0,0,0,0,0,0,0,0,3,0,3,0,0,0,0,3,0,0,0,0,0],
+    [0,2,0,0,0,0,0,0,0,0,0,3,0,0,3,0,0,0,0,0,0,0,0,0,3,0,0,0],
+    [0,0,0,0,0,0,3,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,3,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,3,0,0,0,0,3,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,2,0,0,0,0,0,0,0,0]
 ]
 
@@ -31,44 +33,77 @@ GRID=[
 air=tk.PhotoImage(file="image\Air.png")
 enemy=tk.PhotoImage(file="image\enemy.png")
 point=tk.PhotoImage(file="image\point.png")
-
-def box():
-    x1=0
-    x2=50
-    y1=0
-    y2=50
-    for col in GRID:
-        for row in col:
-            if row==1:
-                canvas.create_image(x2-25,y2-25,image=air)
-            elif row==2:
-                canvas.create_image(x2-25,y2-25,image=enemy)
-            elif row==3:
-                canvas.create_image(x2-25,y2-25,image=point)
-            # else:
-            #     canvas.create_rectangle(x1,y1,x2,y2,fill="white")
-            x1=x2
-            x2+=50
-        x1=0
-        x2=50
-        y1=y2
-        y2+=50
-box()
-# ------------------------countPoint When airplane crash Crystal------
+bgFirst=tk.PhotoImage(file="image\space1.png")
+notLost = True
 number=0
+def box():
+    canvas.create_image(400,400,image=img)
+    x2=50
+    y2=50
+    global notLost, number
+    if notLost and number <10 :
+        for col in GRID:
+            for row in col:
+                if row==1:
+                    canvas.create_image(x2-25,y2-25,image=air)
+                elif row==2:
+                    canvas.create_image(x2-25,y2-25,image=enemy)
+                    # canvas.moveto(x2)
+                elif row==3:
+                    canvas.create_image(x2-25,y2-25,image=point)
+                x2+=50
+            x2=50
+            y2+=50
+    elif number == 10:
+        win()
+    else:
+        lose()
+def bg():
+    canvas.create_text(100,20,text="Space Miner",font=("Pursia",20),fill="white")
+    canvas.create_text(680,650,text="Click here to Play",font=("Pursia",20),fill="white")
+    canvas.create_image(650,400,image=bgFirst)
+bg()
+# ---------------------Button--------------------
+def onClick():
+    box()
+    buttonPlay.pack_forget()
+buttonPlay = tk.Button(root,text="Play",font=("Pursia",15),fg="white",bg="blue",pady=15,padx=100,command=onClick)
+buttonPlay.config(width=7, height=1, bg="#007EE9",fg="yellow",border="2",rounded="2", font=("Arial", 20, "bold"))
+buttonPlay=canvas.create_window(500,250,anchor="nw", window=buttonPlay)
+
+# ------------------------countPoint When airplane crash Crystal------
 canvas.create_text(1250,630,text="Your Score",font=("Pursia",15),fill="white")
 myPoint=canvas.create_text(1250,660,text="0",font=("Pursia",15),fill="white")
 def countPoints():
     global number,myPoint
-    if number<10 :
+    if number<10:
         number+=1
+        winsound .PlaySound("sound\solid.wav",winsound.SND_FILENAME | winsound.SND_ASYNC)
     else:
         canvas.delete("all")
         canvas.create_image(400,400,image=img)
         canvas.create_text(1250,660,text=number,font=("Pursia",15),fill="white")
         canvas.create_text(1250,630,text="Your Score",font=("Pursia",15),fill="white")
 # ---------------------For display Win or Lose-------------
-# def 
+def win():
+    canvas.delete("all")
+    canvas.create_image(400,400,image=img)
+    canvas.create_text(650,350,text="You Win !",font=("Pursia",45),fill="white")
+    canvas.create_text(650,450,text="congratulations",font=("Pursia",15),fill="white")
+    buttonPlay = tk.Button(root,text="start",font=("Pursia",15),fg="white",bg="blue",pady=15,padx=15,command=onClick,tags="PNC")
+    buttonPlay.config(width=7, height=1, bg="#007EE9",fg="yellow",border="2", font=("Arial", 20, "bold"))
+    buttonPlay=canvas.create_window(520,500,anchor="nw", window=buttonPlay)
+    buttonPlay = tk.Button(root,text="cancel",font=("Pursia",15),fg="white",bg="blue",pady=15,padx=15,command=onClick)
+    buttonPlay.config(width=7, height=1, bg="#007EE9",fg="yellow",border="2", font=("Arial", 20, "bold"))
+    buttonPlay=canvas.create_window(700,500,anchor="nw", window=buttonPlay)
+
+def lose():  
+    canvas.delete("all")
+    canvas.create_image(400,400,image=img)
+    canvas.create_text(650,350,text="You Lose!",font=("Pursia",45),fill="white")
+# --------------------------------Button Play Again-------------------
+# def onClick():
+
 # ---------------------------moveright----------------
 def moveRight(event):
     global GRID
@@ -83,14 +118,14 @@ def moveRight(event):
                 GRID[col][row]=0
                 GRID[col][row+1]=1
                 isRight=True
-            elif (GRID[col][row]==1) and (not isRight) and (GRID[col][row+1])==3:
+            elif (GRID[col][row]==1) and (not isRight) and (GRID[col][row+1]==3):
                 GRID[col][row]=0 
                 GRID[col][row+1]=1
                 countPoints()
                 isRight=True
     box()
 root.bind("<Right>",moveRight)
-# ----------------------------moveLeft----------------------------
+    # ----------------------------moveLeft----------------------------
 def moveleft(event):
     global GRID
     canvas.delete("all")
@@ -100,10 +135,10 @@ def moveleft(event):
     isLeft=False
     for col in range(len(GRID)):
         for row in range(len(GRID[col])):
-            if GRID[col][row]==1 and not isLeft and GRID[col][row-1]==0:
+            if GRID[col][row]==1 and not isLeft and GRID[col][row-1]==0 and GRID[col][row-1]!=2:
                 GRID[col][row]=0
                 GRID[col][row-1]=1
-                isRight=True
+                isLeft=True
             elif (GRID[col][row]==1) and (not isLeft) and (GRID[col][row-1]==3):
                 GRID[col][row]=0 
                 GRID[col][row-1]=1
@@ -111,9 +146,9 @@ def moveleft(event):
                 isLeft=True
     box()
 root.bind("<Left>",moveleft)
-# # --------------------moveDown------------------
+    # # --------------------moveDown------------------
 def moveDown(event):
-    global GRID
+    global GRID, notLost
     canvas.delete("all")
     canvas.create_image(400,400,image=img)
     canvas.create_text(1250,630,text="Your Score",font=("Pursia",15),fill="white")
@@ -130,11 +165,15 @@ def moveDown(event):
                 GRID[col+1][row]=1
                 countPoints()
                 isDown=True
+            elif (GRID[col][row]==1) and (GRID[col-1][row]==2) and not isDown:
+                lose()
+                isDown=False
+                canvas.destroy("all")
     box()
 root.bind("<Down>",moveDown)
-# # -------------------------------------moveUp-------------------
+    # # -------------------------------------moveUp-------------------
 def moveUp(event):
-    global GRID,result
+    global GRID
     canvas.delete("all")
     canvas.create_image(400,400,image=img)
     canvas.create_text(1250,630,text="Your Score",font=("Pursia",15),fill="white")
@@ -147,25 +186,18 @@ def moveUp(event):
                 GRID[col][row]=0 
                 GRID[col-1][row]=1
                 isUp=True
-            elif (GRID[col][row]==1) and (not isUp) and (GRID[col-1][row]==3):
+            elif (GRID[col][row]==1) and (GRID[col-1][row]==3) and not isUp:
                 GRID[col][row]=0 
                 GRID[col-1][row]=1
                 countPoints()
                 isUp=True
-            elif (GRID[col][row]==1) and (not isUp) and (GRID[col-1][row]==2) (GRID[col-1][row]!=3) and number==10:
-                GRID[col][row]=0 
-                GRID[col-1][row]=1
+            elif (GRID[col][row]==1) and (GRID[col-1][row]==2) and (GRID[col+1][row]==0) and not isUp:
+                lose()
                 isUp=True
+                canvas.destroy("all")
     # print(isUp)
-    box()
+    box() 
 root.bind("<Up>",moveUp)
-
-# ------------------------------move enemy--------------
-# def onClick():
-#     box()
-#     buttonPlay.pack_forget()
-# buttonPlay = tk.Button(root,text="Play",command=onClick)
-# buttonPlay.pack(expand=True,fill="both")
 # ----------------display------------
 canvas.pack(expand=True,fill="both")
 frame.pack(expand=True,fill="both")
